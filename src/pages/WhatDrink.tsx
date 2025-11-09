dc5dd0b09c7d483ee114eb094c2d32cb40355a48
HEIHEI\hey <kimdan2025@hufs.ac.kr>
2025-11-09 18:55:50 +0900

    feature: 섭취입력페이지 UI 수정

--- src/pages/WhatDrink.tsx
+++ src/pages/WhatDrink.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import React, { useState } from "react";
-import Dropdown from "../pages/DropDown";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import Nav from "../components/nav";
 import bb from "../assets/backbutton.svg";
@@ -8,10 +7,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function WhatDrink() {
   const navigate = useNavigate();
-  const drinkOptions = ["소주", "맥주", "에너지드링크", "커피", "직접 입력"];
-  const [drink, setDrink] = useState("");
-  const [caffaine, setCaffaine] = useState("");
-  const [alcohol, setAlcohol] = useState("");
+
   const [drug, setDrug] = useState("");
   const handleGoBack = () => {
     navigate(-1);
@@ -33,40 +29,6 @@ export default function WhatDrink() {
         <Ht onClick={handleGoToMyPage}>마이페이지</Ht>
       </Header>
       <ContentContainer>
-        <Dropdown
-          label="섭취 음료 종류"
-          selected={drink}
-          options={drinkOptions}
-          onSelect={setDrink}
-        ></Dropdown>
-        <ContainBox>
-          <CaffaineBox>
-            <CaffaineText>카페인 함량</CaffaineText>
-            <CaffaineWrapper>
-              <Caffaine
-                type="number"
-                value={caffaine}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setCaffaine(e.target.value)
-                }
-              ></Caffaine>
-              <Text>mg</Text>
-            </CaffaineWrapper>
-          </CaffaineBox>
-          <CaffaineBox>
-            <CaffaineText>알코올 함량</CaffaineText>
-            <CaffaineWrapper>
-              <Caffaine
-                type="number"
-                value={alcohol}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setAlcohol(e.target.value)
-                }
-              ></Caffaine>
-              <Text>mg</Text>
-            </CaffaineWrapper>
-          </CaffaineBox>
-        </ContainBox>
         <DrugBox>
           <DrugText>현재 복용하고 있는 약</DrugText>
           <DrugLine>
@@ -80,14 +42,26 @@ export default function WhatDrink() {
             <Plus></Plus>
           </DrugLine>
         </DrugBox>
-        <NoTimeBox>
-          <NotimeText>금지 시간 분석</NotimeText>
-          <Notime></Notime>
-        </NoTimeBox>
         <ButtonLine>
           <CaffainePlus onClick={GotoDrinkCaffaine}>카페인 추가</CaffainePlus>
           <AlcoholPlus onClick={GotoDrinkAlcohol}>알코올 추가</AlcoholPlus>
         </ButtonLine>
+        <NoBox>
+          <Notext>금지 시간 분석</Notext>
+          <No></No>
+        </NoBox>
+        <TakenBox>
+          현재 섭취한 음료
+          <CoffeeLine>
+            <Coffee></Coffee>
+            <Coffee>mg</Coffee>
+          </CoffeeLine>
+          <CoffeeLine>
+            <Alcohol></Alcohol>
+            <Alcohol>ml</Alcohol>
+            <Alcohol>%</Alcohol>
+          </CoffeeLine>
+        </TakenBox>
       </ContentContainer>
       <Nav></Nav>
     </Screen>
@@ -132,89 +106,7 @@ const ContentContainer = styled.div`
   left: 15px;
   top: 72px;
 `;
-const ContainBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 0px;
-  gap: 15px;
 
-  width: 363px;
-  height: 67px;
-
-  /* 내부 오토레이아웃 */
-  flex: none;
-
-  flex-grow: 0;
-`;
-const CaffaineBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 0px;
-  gap: 8px;
-
-  width: 174px;
-  height: 69px;
-
-  /* 내부 오토레이아웃 */
-  flex: none;
-
-  flex-grow: 1;
-`;
-const CaffaineText = styled.div`
-  /* 카페인 함량 */
-
-  width: 174px;
-  height: 21px;
-
-  font-family: "Pretendard";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 18px;
-  line-height: 21px;
-
-  color: #333333;
-
-  /* 내부 오토레이아웃 */
-  flex: none;
-
-  align-self: stretch;
-  flex-grow: 0;
-`;
-const CaffaineWrapper = styled.div`
-  position: relative;
-  display: inline-block;
-`;
-const Caffaine = styled.input`
-  position: relative;
-  width: 134px;
-  height: 40px;
-  padding-right: 30px;
-  border: 1.5px solid #ebebeb;
-  border-radius: 5px;
-  padding-left: 10px;
-  /* 내부 오토레이아웃 */
-  flex: none;
-
-  align-self: stretch;
-  flex-grow: 0;
-`;
-const Text = styled.div`
-  position: absolute;
-  width: 26px;
-  height: 21px;
-  right: 15px;
-  top: calc(50% - 21px / 2 - 0.5px);
-  pointer-events: none;
-  font-family: "Pretendard";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 18px;
-  line-height: 21px;
-
-  color: #333333;
-`;
 const DrugBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -294,60 +186,6 @@ const Plus = styled(AiOutlinePlusCircle)`
   border-color: #b6f500;
   cursor: pointer;
 `;
-const NoTimeBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0px;
-  gap: 8px;
-
-  width: 363px;
-  height: 144px;
-
-  /* 내부 오토레이아웃 */
-  flex: none;
-
-  align-self: stretch;
-  flex-grow: 0;
-`;
-const NotimeText = styled.div`
-  width: 103px;
-  height: 21px;
-
-  font-family: "Pretendard";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 18px;
-  line-height: 21px;
-  text-align: center;
-
-  color: #333333;
-
-  /* 내부 오토레이아웃 */
-  flex: none;
-
-  flex-grow: 0;
-`;
-const Notime = styled.div`
-  display: flex;
-  align-items: center;
-
-  gap: 10px;
-  isolation: isolate;
-
-  width: 343px;
-  height: 115px;
-
-  background: #ffffff;
-  border: 1.5px solid #ebebeb;
-  border-radius: 5px;
-
-  /* 내부 오토레이아웃 */
-  flex: none;
-
-  align-self: stretch;
-  flex-grow: 0;
-`;
 const ButtonLine = styled.div`
   display: flex;
   flex-direction: row;
@@ -419,4 +257,136 @@ const AlcoholPlus = styled.button`
   text-align: center;
 
   color: #000000;
+`;
+const NoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  gap: 8px;
+
+  width: 363px;
+  height: 144px;
+
+  /* Inside auto layout */
+  flex: none;
+  order: 2;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+const Notext = styled.div`
+  width: 103px;
+  height: 21px;
+
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 21px;
+  text-align: center;
+
+  color: #333333;
+
+  /* Inside auto layout */
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+const No = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 9px;
+  gap: 10px;
+  isolation: isolate;
+
+  width: 363px;
+  height: 115px;
+
+  background: #ffffff;
+  border: 1.5px solid #ebebeb;
+  border-radius: 5px;
+`;
+const TakenBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 15px;
+
+  width: 363px;
+  height: 131px;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 21px;
+
+  color: #333333;
+`;
+const CoffeeLine = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 15px;
+
+  width: 363px;
+  height: 40px;
+
+  /* Inside auto layout */
+  flex: none;
+  order: 1;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+const Coffee = styled.div`
+  display: flex;
+  padding-right: 10px;
+  justify-content: flex-end;
+  align-items: center;
+
+  width: 154px;
+  height: 40px;
+
+  background: #ffffff;
+  border: 1.5px solid #ebebeb;
+  border-radius: 5px;
+
+  /* Inside auto layout */
+  flex: none;
+  order: 0;
+  flex-grow: 1;
+  /* 커피 */
+  padding-left: 10px;
+
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  /* identical to box height */
+
+  color: #333333;
+`;
+
+const Alcohol = styled.div`
+  width: 101px;
+  height: 40px;
+  display: flex;
+  padding-right: 10px;
+  justify-content: flex-end;
+  align-items: center;
+  background: #ffffff;
+  border: 1.5px solid #ebebeb;
+  border-radius: 5px;
+  right: 10px;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  /* identical to box height */
+
+  color: #333333;
 `;
